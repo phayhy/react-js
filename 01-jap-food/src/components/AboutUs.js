b99b8aeccd3c2b8d513f3358bf1446e5f847c6be
phayhy <phay.hy@gmail.com>
2022-12-07 01:00:25 +0800

    update folder

---
+++ 01-jap-food/src/components/AboutUs.js
@@ -0,0 +1,12 @@
+//create About Us component
+function AboutUs() {
+  return (
+    <section id="about-us">
+      <div className="content">
+        <h1>Japanese so fresh, any fresh you have to fly to Japan</h1>
+      </div>
+    </section>
+  );
+}
+
+export default AboutUs;
